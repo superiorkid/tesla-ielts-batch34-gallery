741467a7470fd45063e34707053af755e1d50b13
superiorkid <mohammad.ilhamuddin@gmail.com>
2026-03-12 06:01:02 +0800

    small fix getbase64imageurl

--- utils/generateBlurPlaceholder.ts
+++ utils/generateBlurPlaceholder.ts
@@ -8,18 +8,21 @@ export default async function getBase64ImageUrl(
   image: ImageProps,
 ): Promise<string> {
   let url = cache.get(image);
-  if (url) {
-    return url;
-  }
+  if (url) return url;
+
   const response = await fetch(
     `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`,
   );
+
   const buffer = await response.arrayBuffer();
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
+  const input = new Uint8Array(buffer);
+
+  const minified = await imagemin.buffer(input, {
     plugins: [imageminJpegtran()],
   });
 
   url = `data:image/jpeg;base64,${Buffer.from(minified).toString("base64")}`;
   cache.set(image, url);
+
   return url;
 }
